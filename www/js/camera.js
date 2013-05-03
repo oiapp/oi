f7dc4f14def9c6b4abc8d4490e2b17440ee1eccc
ukyg9e5r6k7gubiekd6 <ukyg9e5r6k7gubiekd6@mailinator.com>
2013-05-03 15:43:02 +1000

    Add basic output of locations received via geolocation API

--- www/js/camera.js
+++ www/js/camera.js
@@ -1,24 +1,20 @@
 function takePicture(img) {
-        navigator.camera.getPicture(
-                function(imageURI) {
-			// Success
-			img.attr('src', imageURI);
-			img.css({width: '100%', height: '100%'});
-                },
-                function() {
-			// Failure
-			// Following setTimeout works around an iOS bug
-                        setTimeout(
-                                function() {
-                                        console.log('Picture taking failed');
-                                },
-                                0
-                        );
-                },
-                {
-                        destinationType: Camera.DestinationType.FILE_URI,
-			allowEdit: false,
-			saveToPhotoAlbum: false
-                }
-        );
+	navigator.camera.getPicture(function(imageURI) {
+		// Success
+		img.attr('src', imageURI);
+		img.css({
+			width : '100%',
+			height : '100%'
+		});
+	}, function(err) {
+		// Failure
+		// Following setTimeout works around an iOS bug
+		setTimeout(function() {
+			console.log('Picture taking failed: errcode ' + err.code + ': ' + err.message);
+		}, 0);
+	}, {
+		destinationType : Camera.DestinationType.FILE_URI,
+		allowEdit : false,
+		saveToPhotoAlbum : false
+	});
 }
---
+++ www/js/events.js
@@ -0,0 +1,41 @@
+function onDeviceReady(event) {
+	var theLocation, watch = undefined;
+	$('.jqueryOutput').text('Version ' + $.fn.jquery + ' initialised');
+   	$('.jqueryMobileOutput').text('Version ' + $.mobile.version + ' initialised');
+	$('#takePicture').on('click', function() {
+		var elem = $('#thePicture');
+		takePicture(elem);
+	});
+	$('#showContacts').on('click', function() {
+		var elem = $('#theContacts');
+		showContacts(elem);
+	});
+	theLocation = $('#theLocation');
+	function stopWatchingPosition() {
+		if (watch) {
+			navigator.geolocation.clearWatch(watch);
+			watch = undefined;
+		}
+	}
+	function onLocationError(err) {
+		console.log('Error obtaining location: errcode ' + err.code + ': ' + err.message);
+		stopWatchingPosition(); // Cancel existing watch
+		if (err.code === PositionError.PERMISSION_DENIED) {
+			return; //  Fatal error; bail out
+		}
+		startWatchingPosition(); // Restart watch
+	}
+	function onLocationSuccess(position) {
+		console.log("Location success");
+		showLocation(theLocation, position);
+	}
+	function startWatchingPosition() {
+		stopWatchingPosition();
+		watch = navigator.geolocation.watchPosition(onLocationSuccess, onLocationError, {
+			enableHighAccuracy: true,
+			timeout: 1000
+		});
+	}
+	startWatchingPosition();
+}
+$(document).on('deviceready', onDeviceReady);

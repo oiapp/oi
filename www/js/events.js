7c1e167f1a28f01847fe53e3df0b968ddd556c39
matthew <matthew@freya.funkyproductions.org.au>
2013-07-02 14:14:01 +1000

    Add get and put contact buttons. The get button inserts a test contact into the device's contact database.

--- www/js/events.js
+++ www/js/events.js
@@ -1,5 +1,17 @@
 function onDeviceReady(event) {
-	var theLocation, watch = undefined;
+	var
+		theLocation,
+		watch = undefined,
+		contactServerURL = 'http://www.supermatty.com/OIServer/oiserver.php',
+		// contactServerURL = 'http://dev.funkyproductions.org.au/OIServer/oiserver.php',
+		contactOutput = $('#contactOutput'),
+		testContactData = {
+			displayName: 'Test Display Name'
+			,nickname: 'Test Nickname'
+			// ,phoneNumbers: []
+			// ,emails: []
+		}
+	;
 	$('.cordovaVersion').text(device.cordova);
 	$('.jQueryVersion').text($.fn.jquery);
    	$('.jQueryMobileVersion').text($.mobile.version);
@@ -10,6 +22,47 @@ function onDeviceReady(event) {
 	$('#showContacts').on('click', function() {
 		var elem = $('#theContacts');
 		showContacts(elem);
+	});
+	$('#getContact').on('click', function() {
+		$.ajax({
+			dataType: "json",
+			url: contactServerURL,
+			cache: false,
+			contentType: 'application/json',
+			data: {
+				mode: 'get',
+				contactID: 1
+			},
+			success: function(data, textStatus, jqXHR) {
+				var contact;
+				delete data['contactID']; // Avoid over-writing existing contacts
+				contactOutput.text('Got contact: ' + JSON.stringify(data));
+				contact = navigator.contacts.create(data);
+				contact.save();
+			},
+			error: function(jqXHR, textStatus, errorThrown) {
+				console.log('Contact get error: responseText=' + jqXHR.responseText + ', status=' + textStatus + ', errorThrown=' + errorThrown);
+			}
+		});
+	});
+	$('#putContact').on('click', function() {
+		$.ajax({
+			dataType: "json",
+			url: contactServerURL,
+			cache: false,
+			contentType: 'application/json',
+			data: {
+				mode: 'put',
+				contactID: 1,
+				contactData: testContactData
+			},
+			success: function(data, textStatus, jqXHR) {
+				contactOutput.text('Put response: ' + JSON.stringify(data));
+			},
+			error: function(jqXHR, textStatus, errorThrown) {
+				console.log('Contact put error: responseText=' + jqXHR.responseText + ', status=' + textStatus + ', errorThrown=' + errorThrown);
+			}
+		});
 	});
 	theLocation = $('#theLocation');
 	function stopWatchingPosition() {
